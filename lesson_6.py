363c525771f486ab4e9681dbdf71ce79f2e3b2a0
Lirik <lirik.p@gmail.com>
2021-04-08 13:59:19 +0300

    end DZ

--- lesson_6.py
+++ lesson_6.py
@@ -4,19 +4,21 @@ import numpy as np
 from sklearn.tree import DecisionTreeRegressor
 from sklearn import model_selection
 
-
-
 from sklearn.datasets import load_diabetes
+
 X, y = load_diabetes(return_X_y=True)
 X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.25)
+
 
 def gb_predict(X, trees_list, coef_list, eta):
     # Реализуемый алгоритм градиентного бустинга будет инициализироваться нулевыми значениями,
     # поэтому все деревья из списка trees_list уже являются дополнительными и при предсказании прибавляются с шагом eta
-    return np.array([sum([eta* coef * alg.predict([x])[0] for alg, coef in zip(trees_list, coef_list)]) for x in X])
+    return np.array([sum([eta * coef * alg.predict([x])[0] for alg, coef in zip(trees_list, coef_list)]) for x in X])
+
 
 def mean_squared_error(y_real, prediction):
-    return (sum((y_real - prediction)**2)) / len(y_real)
+    return (sum((y_real - prediction) ** 2)) / len(y_real)
+
 
 def bias(y, z):
     return (y - z)
@@ -57,15 +59,16 @@ def gb_fit(n_trees, max_depth, X_train, X_test, y_train, y_test, coefs, eta):
 
 
 def evaluate_alg(X_train, X_test, y_train, y_test, trees, coefs, eta):
-    train_prediction = gb_predict(X_train, trees, coefs, eta)
-
-    print(f'Ошибка алгоритма из {n_trees} деревьев глубиной {max_depth} \
-    с шагом {eta} на тренировочной выборке: {mean_squared_error(y_train, train_prediction)}')
-
+    # train_prediction = gb_predict(X_train, trees, coefs, eta)
+    #
+    # print(f'Ошибка алгоритма из {n_trees} деревьев глубиной {max_depth} \
+    # с шагом {eta} на тренировочной выборке: {mean_squared_error(y_train, train_prediction)}')
+    #
     test_prediction = gb_predict(X_test, trees, coefs, eta)
 
     print(f'Ошибка алгоритма из {n_trees} деревьев глубиной {max_depth} \
     с шагом {eta} на тестовой выборке: {mean_squared_error(y_test, test_prediction)}')
+
 
 def get_error_plot(n_trees, train_err, test_err):
     plt.xlabel('Iteration number')
@@ -76,6 +79,7 @@ def get_error_plot(n_trees, train_err, test_err):
     plt.legend(loc='upper right')
     plt.show()
 
+
 # Число деревьев в ансамбле
 n_trees = 10
 # для простоты примем коэффициенты равными 1
@@ -85,27 +89,33 @@ max_depth = 3
 # Шаг
 eta = 1
 
-trees, train_errors, test_errors = gb_fit(n_trees, max_depth, X_train, X_test, y_train, y_test, coefs, eta)
-evaluate_alg(X_train, X_test, y_train, y_test, trees, coefs, eta)
-get_error_plot(n_trees, train_errors, test_errors)
-
-n_trees = 100
-coefs = [1] * n_trees
-trees, train_errors, test_errors = gb_fit(n_trees, max_depth, X_train, X_test, y_train, y_test, coefs, eta)
-evaluate_alg(X_train, X_test, y_train, y_test, trees, coefs, eta)
-get_error_plot(n_trees, train_errors, test_errors)
-
-eta = 0.1
-trees, train_errors, test_errors = gb_fit(n_trees, max_depth, X_train, X_test, y_train, y_test, coefs, eta)
-evaluate_alg(X_train, X_test, y_train, y_test, trees, coefs, eta)
-get_error_plot(n_trees, train_errors, test_errors)
-
-eta = 0.01
-trees, train_errors, test_errors = gb_fit(n_trees, max_depth, X_train, X_test, y_train, y_test, coefs, eta)
-evaluate_alg(X_train, X_test, y_train, y_test, trees, coefs, eta)
-get_error_plot(n_trees, train_errors, test_errors)
-
-
 # 1) Для реализованной модели градиентного бустинга построить графики зависимости ошибки
 # от количества деревьев в ансамбле и от максимальной глубины деревьев.
 # Сделать выводы о зависимости ошибки от этих параметров.
+
+
+max_n_trees = 14
+np_data = np.zeros((max_n_trees,max_depth))
+
+for depth in range(max_depth):
+    n_trees = 1
+    while n_trees <= max_n_trees:
+        coefs = [1] * n_trees
+        trees, train_errors, test_errors = gb_fit(n_trees, depth+1, X_train, X_test, y_train, y_test, coefs, eta)
+        np_data[n_trees-1,depth] = mean_squared_error(y_test, gb_predict(X_test, trees, coefs, eta))
+
+        n_trees+=1
+    print(f'DEPTH: {depth+1}')
+
+plt.xlabel('Iteration number')
+plt.ylabel('MSE')
+plt.xlim(0, max_n_trees)
+for depth in range(max_depth):
+    plt.plot(list(range(max_n_trees)), np_data[:,depth], label=f'depth_{depth+1}')
+
+plt.legend(loc='upper right')
+plt.show()
+
+
+# Ответ(философский вывод): Судя по графику ошибки MSE, от 2х до 4х дереревьев достаточно для получения минимальной ошибки
+# Если рассматривать глубину деревьев, то для данного алгоритма пеньки лучьше чем ветвистые деревья. Это легко проследить по синему графику depth_1
